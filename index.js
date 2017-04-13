7d29eca8b32f990496bf7c854eb86f1026838b4d
Eric Pan <epan@users.noreply.github.com>
2017-04-12 20:12:37 -0700

    Specify GET for getting all phrases

--- index.js
+++ index.js
@@ -7,10 +7,24 @@ const IP = '127.0.0.1'
 const server = http.createServer(function (request, response) {
 
   if (request.url === '/phrases') {
-    // return JSON of all the phrases
-    response.setHeader('Content-Type', 'application/json;charset=utf-8')
-    response.writeHead(200)
-    response.end(JSON.stringify(phrases))
+    // GET /phrases return JSON of all the phrases
+    if (request.method === 'GET') {
+      response.setHeader('Content-Type', 'application/json;charset=utf-8')
+      response.writeHead(200)
+      response.end(JSON.stringify(phrases))
+    }
+    // POST /phrases add a new phrase
+    if (request.method === 'POST') {
+      // listen for all the data
+      // once we have all the data
+        // concat chunks together
+        // add id to the phrase (using length of phrases array)
+        // add phrase to phrase collection
+
+    }
+
+    // GET /phrases:id, get phrase by id
+  
   }
 
   console.log('the server heard a request')
