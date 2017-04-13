9e556d6a16abd0ea5df5fb5a4bb895142716558c
Eric Pan <epan@users.noreply.github.com>
2017-04-12 21:03:30 -0700

    Set up home page route and POST new phrase

--- index.js
+++ index.js
@@ -1,34 +1,58 @@
 const http = require('http')
+const fs = require('fs')
+
 const phrases = require('./data/phrases')
 
 const PORT = 1337
 const IP = '127.0.0.1'
 
-const server = http.createServer(function (request, response) {
+const server = http.createServer((request, response) => {
 
   if (request.url === '/phrases') {
+
     // GET /phrases return JSON of all the phrases
     if (request.method === 'GET') {
       response.setHeader('Content-Type', 'application/json;charset=utf-8')
       response.writeHead(200)
       response.end(JSON.stringify(phrases))
     }
+
     // POST /phrases add a new phrase
     if (request.method === 'POST') {
-      // listen for all the data
-      // once we have all the data
-        // concat chunks together
-        // add id to the phrase (using length of phrases array)
-        // add phrase to phrase collection
+      let data = ''
+      
+      request.on('data', function (chunk) {
+        data += chunk
+      })
+
+      request.on('end', function () {
+        let newPhrase = JSON.parse(data)
+        newPhrase.id = phrases.length
+        phrases.push(newPhrase)
+
+        // close out response cycle
+        response.writeHead(201)
+        response.end(`Added ${newPhrase.korean}`)
+      })
 
     }
 
     // GET /phrases:id, get phrase by id
-  
+    
+  // GET /, serve up index.html
+  } else if (request.url === '/' && request.method === 'GET') {
+    fs.readFile(__dirname + '/client/index.html', (err, data) => {
+      if (err) { console.error(err) }
+
+      response.setHeader('Content-Type', 'text/html;charset=utf-8')
+      response.writeHead(200)
+      response.end(data)
+    })
+  } else {
+    console.log('the server heard a request')
+    response.end('here is the response')
   }
 
-  console.log('the server heard a request')
-  response.end('here is the response')
 })
 
 console.log(`firing up the server at ${IP}:${PORT} ...`)
