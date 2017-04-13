94ddb746d74b8c7e4f9c688f56832a955fbd4963
Eric Pan <epan@users.noreply.github.com>
2017-04-12 17:06:12 -0700

    server connected

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http')
+
+const PORT = 1337
+const IP = '127.0.0.1'
+
+const server = http.createServer(function (request, response) {
+  console.log('the server heard a request')
+  response.end('here is the response')
+})
+
+console.log(`firing up the server at ${IP}:${PORT} ...`)
+server.listen(PORT, IP)
