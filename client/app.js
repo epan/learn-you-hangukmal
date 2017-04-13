7e84a310b1dffa4c77f63f54e90fa211bf55fb6a
Eric Pan <epan@users.noreply.github.com>
2017-04-12 21:53:33 -0700

    Show all phrases on button click

--- client/app.js
+++ client/app.js
@@ -1,1 +1,23 @@
 console.log('learn yewww kr')
+$('#show-all').on('click', () => {
+  $.ajax({
+    method: 'GET',
+    url: 'http://127.0.0.1:1337/phrases'
+  })
+  .done((phrases) => {
+    renderPhrases(phrases)
+  })
+})
+
+function renderPhrases(phrases) {
+  $('#phrases').html('')
+  phrases.forEach((phrase) => {
+    renderPhrase(phrase)
+  })
+}
+
+function renderPhrase(phrase) {
+  var $el = $('<div class="phrase"></div>')
+  $el.text(`English: ${phrase.english} || Korean: ${phrase.korean}`)
+  $('#phrases').append($el)
+}
