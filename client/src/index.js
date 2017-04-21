12a828b3bf31865c379d3861f5f59c1c05dd1c45
Eric Pan <epan@users.noreply.github.com>
2017-04-20 21:08:44 -0700

    Add Webpack watch and React template

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,8 @@
-console.log('inside of web pack jay es');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+  <h1>annyeong something else from react!</h1>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
